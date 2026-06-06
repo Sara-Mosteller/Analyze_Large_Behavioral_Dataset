dedfcc2fbdae7412a96ebb2a8b6f4420ca5df945
Sara-Mosteller <194526956+Sara-Mosteller@users.noreply.github.com>
2026-06-06 15:44:03 +0100

    combine datasets

--- D_Combined_Dataset/S7_Combine_trial_datasets.R
+++ D_Combined_Dataset/S7_Combine_trial_datasets.R
@@ -3,6 +3,13 @@
 #Also reformats and saves the experiments in dataset 1 
 ###########################################################################################################
 
+#Import the needed packages
+
+#install.packages(dplyr)
+library(dplyr)
+#install.packages(tidyverse)
+library(tidyverse)
+
 #Read and store the datasets as dataframes
 
 #Dataset 1, expt 1, (Xu et al., 2018, Expt 1)
@@ -65,6 +72,7 @@ dim(dataset1_expt2) #Should be 293880 rows and 48 columns
 dataset1_expt1$study <- 1 #Xu et al., 2018
 dataset1_expt1$experiment <- 1 #Expt 1
 dataset1_expt1$session <- 1 
+dataset1_expt1$rt <-  dataset1_expt1$rt*1000
 dataset1_expt1$distance_from_monitor <- 60 #ms
 dataset1_expt1$stim_size <- 1.775 #average degrees of visual angle
 dataset1_expt1$fixation_size <- 0.415 #average degrees of visual angle
@@ -116,16 +124,8 @@ dataset1_expt1 <- subset(dataset1_expt1, select = -id)
 dataset1_expt1$id <- dataset1_expt1$id_renumbered
 dataset1_expt1 <- subset(dataset1_expt1, select = -id_renumbered)
 
-#For a trimmed version of the dataset, eliminate all but the first 160 trials, so that there are 120 trials of set size 4 and 8
-dataset1_expt1_trimmed <-  dataset1_expt1 %>% group_by(id) %>% slice_head(n = 160) %>% ungroup()
-
-#Select the overlapping columns
-dataset1_expt1_trimmed <- subset(dataset1_expt1_trimmed, select = c('study', 'experiment', 'id', 'session', 'trial_num', 'block', 'trial_num_within_block', 'set_size', 'change', 'response', 'rt', 'fixation_size', 'stim_size', 'stim_duration', 'retention_interval', 'distance_from_monitor', 'test_location_x_from_center', 'test_location_y_from_center'))
-
 #Check the number of unique IDs
 length(unique(dataset1_expt1$id)) #should be 135
-
-dim(dataset1_expt1_trimmed) #Should be 21600 rows and 17 columns
 
 #Write the full csv file 
 
@@ -156,6 +156,7 @@ dataset1_expt2$break_duration <- 6000 #ms
 dataset1_expt2$distance_from_monitor <- 60 #ms
 dataset1_expt2$study <- 1 #Xu et al., 2018
 dataset1_expt2$experiment <- 2 #Expt 2
+dataset1_expt2$rt <-  dataset1_expt2$rt*1000
 
 #create the final location variables
 dataset1_expt2$test_location_x_from_center <- dataset1_expt2$test_location_x - 681 #pixels from the mean
@@ -199,13 +200,6 @@ dataset1_expt2 <- dataset1_expt2 %>% arrange(by = id, day)
 
 dataset1_expt2$session <- rep(1:31, times = 79, each = 120) #because there are 31 sessions per participant and 120 trials per session
 
-#Eliminate all but the first session (120 trials on the first day and 48 trials on the second day)
-dataset1_expt2_trimmed <- dataset1_expt2 %>% group_by(id) %>% slice_head(n = 168) %>% ungroup()
-
-#Select the overlapping columns
-dataset1_expt2_trimmed <- subset(dataset1_expt2_trimmed, select = c('study', 'experiment', 'id', 'session', 'trial_num', 'block', 'trial_num_within_block', 'set_size', 'change', 'response', 'rt', 'fixation_size', 'stim_size', 'stim_duration', 'retention_interval', 'distance_from_monitor', 'test_location_x_from_center', 'test_location_y_from_center'))
-
-dim(dataset1_expt2_trimmed) #Should be 13272 rows out of the original 293880
 #Check the number of unique IDs
 length(unique(dataset1_expt2$id)) #should be 79
 
@@ -247,57 +241,32 @@ dataset2 = subset(dataset2, select = c('study', 'experiment', 'id', 'session', '
 length(unique(dataset2$id)) #should be 3838 ids
 
 ###########################################################################################################
-#Create and write the combined datasets
+#Create and write the combined dataset
 ###########################################################################################################
 
-#Create a version with the trimmed datasets
-data <- rbind(dataset1_expt1_trimmed, dataset1_expt2_trimmed, dataset2)
-#Create the unique id for each dataset in the combined file
-data$id <- as.numeric(data$id)
-data <- data %>% ungroup() %>%
-  mutate(group = cumsum(id != lag(id, default = first(id))) + 1)
-
-#Delete the original id column and replace it with the group column
-data <- subset(data, select = -c(id))
-data$id <- data$group
-data <- subset(data, select = -c(group))
-
-#Select and order the columns to keep
-data_final <- subset(data, select = c('study', 'experiment', 'id', 'session', 'trial_num','set_size', 'change', 'response', 'rt', 'stim_size', 'fixation_size', 'stim_duration', 'retention_interval', 'distance_from_monitor', 'test_location_x_from_center', 'test_location_y_from_center'))
-head(data_final)
-#Check the dimensions
-dim(data_final) #495768 rows, 16 columns
-#Check the number of unique IDs
-length(unique(data_final$id)) #should be 4052 ids
-
-#Write the trimmed, combined csv file
-write_csv(data_final, 'Combined_trial_data_with_120_4or8_trials_per_individual_dataset.csv')
-
-
-#Create a version with the full datasets
 dataset1_expt1_subset <- subset(dataset1_expt1, select = c('study', 'experiment', 'id', 'session', 'trial_num', 'block', 'trial_num_within_block','set_size', 'change', 'response', 'rt', 'fixation_size', 'stim_size', 'stim_duration','retention_interval', 'distance_from_monitor', 'test_location_x_from_center', 'test_location_y_from_center'))
 dataset1_expt2_subset <- subset(dataset1_expt2, select = c('study', 'experiment', 'id', 'session', 'trial_num', 'block', 'trial_num_within_block','set_size', 'change', 'response', 'rt', 'fixation_size', 'stim_size', 'stim_duration','retention_interval', 'distance_from_monitor', 'test_location_x_from_center', 'test_location_y_from_center'))
 dataset2_subset <- subset(dataset2, select = c('study', 'experiment', 'id', 'session', 'trial_num', 'block', 'trial_num_within_block','set_size', 'change', 'response', 'rt', 'fixation_size', 'stim_size', 'stim_duration','retention_interval', 'distance_from_monitor', 'test_location_x_from_center', 'test_location_y_from_center'))
 
 
-data_full <- rbind(dataset1_expt1_subset, dataset1_expt2_subset, dataset2_subset)
-data_full$block[is.na(data_full$block)] <- 1
+data_combined <- rbind(dataset1_expt1_subset, dataset1_expt2_subset, dataset2_subset)
+data_combined$block[is.na(data_combined$block)] <- 1
 #Create the unique id for each dataset in the combined file
-data_full$id <- as.numeric(data_full$id)
-data_full <- data_full %>% ungroup() %>%
+data_combined$id <- as.numeric(data_combined$id)
+data_combined <- data_combined %>% ungroup() %>%
   mutate(group = cumsum(id != lag(id, default = first(id))) + 1)
 
 #Delete the original id column and replace it with the group column
-data_full <- subset(data_full, select = -c(id))
-data_full$id <- data_full$group
-data_full <- subset(data_full, select = -c(group))
+data_combined <- subset(data_combined, select = -c(id))
+data_combined$id <- data_combined$group
+data_combined <- subset(data_combined, select = -c(group))
 
 # Move the id col to the right place
-data_full_final <- data_full %>% relocate(id, .after = experiment)
+data_final <- data_combined %>% relocate(id, .after = experiment)
 
-head(data_full_final)
-dim(data_full_final) #Should be 827646 rows and 18 columns
-length(unique(data_full_final$id)) #should be 4052 ids
+head(data_final)
+dim(data_final) #Should be 827646 rows and 18 columns
+length(unique(data_final$id)) #should be 4052 ids
 
-#Write the trimmed, combined csv file
-write_csv(data_full_final, 'Combined_trial_data_with_all_trials_in_all_datasets.csv')
+#Write the combined csv file
+write_csv(data_final, 'Combined_trial_data.csv')
